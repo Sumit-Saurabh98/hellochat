3a89386397626e078376676e603e771930479546
Sumit Saurabh <macuser@macs-MacBook-Air.local>
2025-08-10 04:17:41 -0700

    authemtication completed

--- backend/chat/dist/index.js
+++ backend/chat/dist/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
 // routes
 app.use("/api/v1", chatRoutes);
--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
 
@@ -8,6 +9,8 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
